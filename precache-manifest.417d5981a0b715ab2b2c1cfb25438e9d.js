51b9ea1d0be9fa1f2769779fcf061a3d4a20de8d
Trent Nguyen <trent.nguyen@ideas.com>
2020-02-24 14:20:25 -0600

    Updates

---
+++ precache-manifest.417d5981a0b715ab2b2c1cfb25438e9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4253c21e366c0e4d75f31ec1ddf177b0",
+    "url": "/mw-battlepass-tracker/index.html"
+  },
+  {
+    "revision": "4ac4d5923ae516f99760",
+    "url": "/mw-battlepass-tracker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a2a98e82d2403ea3ae06",
+    "url": "/mw-battlepass-tracker/static/js/2.7adea31e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/mw-battlepass-tracker/static/js/2.7adea31e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac4d5923ae516f99760",
+    "url": "/mw-battlepass-tracker/static/js/main.9bdac940.chunk.js"
+  },
+  {
+    "revision": "c3a2463f15e963567a1c",
+    "url": "/mw-battlepass-tracker/static/js/runtime-main.217bd898.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mw-battlepass-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
