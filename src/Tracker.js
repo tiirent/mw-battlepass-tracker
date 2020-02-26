6a47ea4f01c6dae9910acc01e889b4f9a97679cd
Trent Nguyen <trent.nguyen@ideas.com>
2020-02-26 14:52:59 -0600

    pulsing progress bar

--- src/Tracker.js
+++ src/Tracker.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar'
+import './scss/Tracker.scss'
 
 var startDate = new Date(2020, 3, 8);
 var endDate = new Date(2020, 5, 3);
@@ -14,11 +15,8 @@ class Tracker extends React.Component {
 
   render() {
     return (
-      <div>
-        <header>
-          {this.state.level}
-          <TrackerBar />
-        </header>
+      <div className = "Tracker">
+        <TrackerBar />
       </div>
     );
   }
@@ -33,26 +31,44 @@ class Tracker extends React.Component {
 }
 
 class TrackerBar extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      level: getLevel()
+      level: getLevel(),
+      margin: 0,
+      value: parseInt(getLevel())
     }
   }
 
   render() {
     return (
       <div>
-        <ProgressBar striped animated variant="dark" now = {this.state.level} />
+        <ProgressBar striped animated variant="dark" now = {this.state.value} label={this.state.level} />
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({level: getLevel()}), 1000);
+    this.interval2 = setInterval(() => this.updateValue(), 50);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+    clearInterval(this.interval2);
+  }
+
+  updateValue() {
+    this.setState({margin: this.state.margin + Math.PI/36});
+    this.setState({value: parseInt(this.state.level) + Math.sin(this.state.margin)});
   }
 }
 
 function getLevel() {
   var totalTime = endDate - startDate;
   var currentTime = Date.now() - startDate;
-  return Math.abs(100 * (currentTime / totalTime)).toFixed(5);
+  return Math.abs(100 * (currentTime / totalTime)).toFixed(2);
 }
 
 export default Tracker;
